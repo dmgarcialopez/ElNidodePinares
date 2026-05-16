1fb7f59216b6b8e617d8e60221e305e006287647
dmgarcialopez <dmgarcia.lopez@gmail.com>
2026-05-16 09:46:16 +0200

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,11 @@
+export const CONFIG = {
+    CAPTURE_RANGE: 100000, // Ajustado a metros reales (en tu original tenías 100km para test)
+    GAME_TICK_MS: 3000,
+    COLOR_NAV: '#2196F3',
+    URLS: {
+        pois: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQhcyPAmVSPAxciRkNPergJLMSIwcTWSZiQKNkWcJDmzjvGp1xK8v9Ho2MNOWL8P6meG5GWr9JGueme/pub?gid=0&output=csv',
+        bici: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRAqPw_lnSBWloX_sMUUBgUIouLYnQVDtkK5BZrEPUyp4KuAceto_9CJYHFjDYJSNkYLOksqsiz7mB-/pub?gid=0&output=csv',
+        paseo: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSxOG367O04AVZmVsikJkcsU0cho0R1RAkDdcvxrLZ1XRiq-wXTl0RN8aEzaRfS57ZHUJn-OB4r_TDa/pub?gid=0&output=csv',
+        duendes: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTwHHhTmV0CvkquPjDRyco_59SNSnIWgBsHVtpjfJsonJU47XvREHMBjLxIeHU7-dbdEkFGYE8_MwKp/pub?output=csv'
+    }
+};
